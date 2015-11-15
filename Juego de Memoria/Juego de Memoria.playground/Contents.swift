bdbb1df4f0fe2bffd464697ea4c0a715ac572ef9
Guillermo Barrientos González <GBG7@macbook-pro-de-guillermo.local>
2015-11-15 13:15:33 -0600

    prueba tres

---
+++ Juego de Memoria/Juego de Memoria.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var i=0;  i<=100; i++ {
+    
+    if i%5 == 0{
+        print("\(i) Bingo!!!" )
+    }
+    if i%2 == 0{
+        print("\(i) par!!!")
+    }
+    if i%2==1{
+        print("\(i) impar!!!")
+    }
+    if i>=30 && i<=40{
+        print("\(i) Viva Swift!!!")
+    }
+    
+}
