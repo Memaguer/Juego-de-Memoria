fb3db541fa255cceef06a160c5ea83bf0e5ce3f5
Guillermo Barrientos González <GBG7@macbook-pro-de-guillermo.local>
2015-11-15 12:35:47 -0600

    Initial Commit

---
+++ Juego de Memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Juego de Memoria
+//
+//  Created by Guillermo Barrientos González on 15/11/15.
+//  Copyright © 2015 Guillermo Barrientos González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
